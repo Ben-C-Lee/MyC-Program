aac01f5d5f7990055192555a94d1b85cf0669633
Ben-C-Lee <1033460206@qq.com>
2016-09-05 17:45:48 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApplication32
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tips";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "检索";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(22, 63);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(507, 54);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(444, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "清空结果";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(5, 123);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(675, 150);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(684, 282);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TX查询器1.0";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication32
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn; //连接对象
+        DataSet ds;         //结果存储容器
+        SqlDataAdapter sda; //执行命令对象
+        SqlCommand cmd;     //命令
+        #region 略略略略略
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Hide();
+            button2.Hide();
+            label1.Text = "输入要查询的编码（股票码、公司码、天相码或券码）";
+            string str = "server=txic_kernel;database=txstock;uid=tx_reader;pwd=123456";
+            try
+            {
+                conn = new SqlConnection(str);
+                richTextBox1.Text = "数据库已连接成功！\n";
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text = ex.Message;
+            }
+            textBox1.Text = "输入要查询的编码，请输入纯数字码";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//检索按钮
+        {
+            //button2_Click(sender, e);
+            dataGridView1.DataSource = null;
+            string Code = textBox1.Text.Trim();
+            Boolean wow = check(Code);//不包含两端的空格
+            if (wow)
+            {
+                richTextBox1.Text += "\n以下是包含该编码的查询结果";
+                TrySearch(Code);
+            }
+            else
+                richTextBox1.Text += "\n输入有误";
+            button2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)//清空结果
+        {
+            richTextBox1.Text = "";
+            textBox1.Text = "输入要查询的编码，请输入纯数字码";
+            button2.Hide();
+            dataGridView1.Hide();
+        }
+        private Boolean check(string InputStr)
+        {
+            int length = InputStr.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (57 < InputStr[i] || InputStr[i] < 48)
+                    return false;
+            }
+            return true;
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                button1_Click(sender, e);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+                button2_Click(sender, e);
+        }
+        #endregion
+        private void search(string Code)
+        {
+            String Wtf=
+                "declare @Code int set @Code="+Code.ToString()+ " select d.f_institution_id "
+                +" as '公司编码',f.f_chn_short_name as '公司名称',e.f_market_code as '股票编码',e.f_chn_short_name as '股票名称',b.f_security_id as '股票券码'"
+                + ",'交易状态'=(case b.f_status when 0 then '正常' when 1 then '发行未上市' when 2 then '暂停上市' when 3 then '摘牌' when 4 then '发行失败' else 'null' end)"
+                + ",a.f_trans_obj_id as '天相交易实体码',a.f_close as '收盘价',a.f_trade_date as '日期'"
+                + " from txstock.dbo.t_stock_price as a inner join txstock.dbo.t_stock_trans_obj as b on a.f_data_state>0 and b.f_data_state>0 and a.f_trans_obj_id=b.f_trans_obj_id"
+                + " and a.f_trade_date=(select MAX(t.f_trade_date) from txstock.dbo.t_stock_price as t where t.f_trans_obj_id=a.f_trans_obj_id)"
+                + " and a.f_trade_date between ISNULL(b.f_start_date,19491001) and ISNULL(b.f_end_date,29991231) left join txstock.dbo.t_common_stock as c"
+                + " on c.f_data_state>0 and c.f_security_id=b.f_security_id left join txstock.dbo.t_corp_basic as d on d.f_data_state>0 and d.f_institution_id=c.f_institution_id"
+                + " inner join txpublic.dbo.t_trans_obj as e on e.f_data_state>0 and e.f_trans_obj_id= a.f_trans_obj_id and a.f_trade_date between ISNULL(b.f_start_date,19491001) and ISNULL(b.f_end_date,29991231)"
+                + " left join txstock.dbo.t_inst_rename  as f on f.f_data_state>0and f.f_institution_id=d.f_institution_id"
+                + " and a.f_trade_date between ISNULL(f.f_start_date,19491001) and ISNULL(f.f_end_date,29991231)"
+                + " where d.f_institution_id=@Code or e.f_market_code=@Code or b.f_security_id=@Code or a.f_trans_obj_id=@Code order by e.f_market_code"
+            ;
+            cmd = new SqlCommand(Wtf,conn);
+            sda = new SqlDataAdapter();
+            sda.SelectCommand = cmd;
+            ds = new DataSet();
+            sda.Fill(ds,"cs");
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void TrySearch(string Code)
+        {
+            try
+            {
+                search(Code);
+                dataGridView1.Show();
+            }
+            catch(Exception ex)
+            {
+                richTextBox1.Text += ex.Message;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
